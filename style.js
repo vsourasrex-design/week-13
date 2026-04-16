0eecb981958264d8e37c2c9d8d8dd3676fa414e7
vsourasrex-design <vsourasrex@gmail.com>
2026-04-16 17:11:15 -0500

    Update style.js

--- style.js
+++ style.js
@@ -1,1 +1,25 @@
 
+
+const textBtn = document.getElementById("textBtn");
+const title = document.getElementById("title");
+const darkModeBtn = document.getElementById("darkModeBtn");
+const myImage = document.getElementById("myImage");
+
+
+textBtn.addEventListener("click", function() {
+  title.textContent = "You clicked the button!";
+});
+
+
+darkModeBtn.addEventListener("click", function() {
+  document.body.classList.toggle("dark-mode");
+});
+
+
+myImage.addEventListener("mouseover", function() {
+  myImage.src = "image2.jpg";
+});
+
+myImage.addEventListener("mouseout", function() {
+  myImage.src = "image1.jpg";
+});
